46b3aff9c6374a7800416468bd39ccb95ec69552
Minhtri Tran <mtran@eg.poly.edu>
2016-04-22 13:26:40 -0400

    Set up initial project

---
+++ LineOfActions/LineOfActions/LineOfActions.cpp
@@ -0,0 +1,13 @@
+// LineOfActions.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world" << endl;
+    return 0;
+}
+
